b733cc8ab8978c8df8ed7016c65bd4cebe137975
gbull122 <gbull122@gmail.com>
2022-10-03 20:02:46 +0100

    Added custom bootstrapper
    Work in progress

---
+++ src/CustomBootstrapper/Bootsrapper.cs
@@ -0,0 +1,33 @@
+﻿namespace CustomBootstrapper
+{
+    using Microsoft.Tools.WindowsInstallerXml.Bootstrapper;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Windows.Interop;
+    using System.Windows.Threading;
+
+    public class Bootsrapper : BootstrapperApplication
+    {
+        protected override void Run()
+        {
+            Engine.Log(LogLevel.Verbose, "Custom Bootstrapper Run Start.");
+            
+
+            Engine.Detect();
+            if (Command.Display == Display.Passive || Command.Display == Display.Full)
+            {
+                var view = new MainView();
+                var vm = new MainViewModel(this);
+                view.DataContext = vm;
+                view.Show();
+            }
+            Dispatcher.Run();
+
+            Engine.Quit(0);
+            Engine.Log(LogLevel.Verbose, "Custom Bootstrapper Run End.");
+        }
+    }
+}
---
+++ src/CustomBootstrapper/MainViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace CustomBootstrapper
+{
+    using Microsoft.Tools.WindowsInstallerXml.Bootstrapper;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class MainViewModel
+    {
+
+        public MainViewModel(BootstrapperApplication bootstrapperApp)
+        {
+
+        }
+    }
+}
